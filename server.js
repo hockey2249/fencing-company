75adb71f9b3c44b0677ceaf61eb3f1633a6bc5a7
hockey2249 <kgibbons14@gmail.com>
2016-11-14 20:15:27 -0700

    Removed fbook logo

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 mailer.extend(app, {
-  from: 'highermarkets@gmail.com',
+  from: 'roman@denverfencespecialists.com',
   host: 'smtp.gmail.com',
   secureConnection: true, 
   port: 465, 
